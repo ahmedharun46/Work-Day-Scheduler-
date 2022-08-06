37654552f4ef25cb0df3c388222bc89a925d9da2
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-08-06 11:24:39 -0500

    HTML updated

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+var dailytasks=[]
+var currentHour= moment().format('h');
+console.log(currentHour);
+function colorChange() {
+    $('span').each(function(){
+        
+    })
+}
+//need a variable to keep track of the current hour with momentjs
+//variable holds the value of the spans id. 
+//If statment to compare two varaible change classes for the current span area 
+
+
+//date display 
+//function need for color of time box depedning on time 
+//function for saving the task then push to global array 
+//eventlistner for button to save timebox
+// function to get items from localstorage 
+
+
