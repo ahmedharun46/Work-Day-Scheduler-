6641d4589635b7b42c449e414fe41220017f43f9
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-08-06 13:09:32 -0500

    Comments added

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@ $("#currentDay").text(moment().format('dddd, MMMM Do'));
 //Current time hour
 var currentHour= moment().format('h');
 console.log(currentHour);
+//Save Click works saves textarea and hour
 $(".saveBtn").on("click",function(){
     var userEntry = $(this).siblings("textarea").val()
     var hour = $(this).parent().attr("id")
@@ -12,7 +13,8 @@ $(".saveBtn").on("click",function(){
 })
 
 
-
+//Local storage item and saves item when refreshed
+//else if to change class depending on time 
 var currentHourBlock = moment().hours()
 console.log(currentHourBlock,currentHour)
 for (i=9;i<18;i++){
